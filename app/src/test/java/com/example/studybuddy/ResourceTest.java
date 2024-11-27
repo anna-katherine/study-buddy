cdc220ac0442dd27866f07a3c563aa50c19280f6
Anna Katherine Zhao <annakath@usc.edu>
2024-11-26 21:00:02 -0800

    1 whitebox test for resource titles

--- app/src/test/java/com/example/studybuddy/ResourceTest.java
+++ app/src/test/java/com/example/studybuddy/ResourceTest.java
@@ -7,13 +7,22 @@ import android.net.Uri;
 
 import org.junit.Test;
 
-public class ResourceTest {
-
+public class ResourceTest
+{
     @Test
-    public void testGetTitle() {
+    public void testGetTitle()
+    {
         Uri mockUri = mock(Uri.class);
         Resource resource = new Resource("Sample Title", mockUri);
         assertEquals("Sample Title", resource.getTitle());
+
+        Uri mockUri2 = mock(Uri.class);
+        Resource resource2 = new Resource(" ", mockUri2);
+        assertEquals(" ", resource2.getTitle());
+
+        Uri mockUri3 = mock(Uri.class);
+        Resource resource3 = new Resource("Testing with Special Characters!@#$%^&*(", mockUri3);
+        assertEquals("Testing with Special Characters!@#$%^&*(", resource3.getTitle());
     }
 
 }
